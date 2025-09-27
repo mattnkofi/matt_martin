fdc58a6e29a5621672698c2d296fe31b2f00ebde
mattnkofi <justinebolanos1018@gmail.com>
2025-09-27 13:57:25 +0800

    second commit

--- app/config/database.php
+++ app/config/database.php
@@ -75,11 +75,11 @@ $database['main'] = array(
 // localhost database
 $database['main'] = array(
     'driver'    => 'mysql',
-    'hostname'    => 'localhost',
+    'hostname'    => 'sql12.freesqldatabase.com',
     'port'        => '3306',
-    'username'    => 'root',
-    'password'    => '',
-    'database'    => 'mockdata',
+    'username'    => 'sql12800273',
+    'password'    => '1Ynybv3TRe',
+    'database'    => 'sql12800273',
     'charset'    => 'utf8mb4',
     'dbprefix'    => '',
     // Optional for SQLite
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '06a515b6e25ede755261e9c2e336bf94a2464dd8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '06a515b6e25ede755261e9c2e336bf94a2464dd8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
